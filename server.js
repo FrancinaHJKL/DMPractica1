8a848cafbe0e4bd4130e68aa5be12310ca7b762f
FrancinaHJKL <isispfx@gmail.com>
2025-06-19 19:35:13 -0600

    se realiza el api rest

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-const express = require('express');
-const app = express();
-const PORT =3000;
+const app = require('./app/app')
+const config = require('./app/config/configuracion')
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);
+app.listen(config.PORT,()=>{
+    console.log(`Server is running on port ${config.PORT}`);
 })
