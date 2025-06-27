fa6f3b7bc5dfc8239f078fb7f16a04c3af185717
FrancinaHJKL <isispfx@gmail.com>
2025-06-26 19:36:02 -0600

    creacion de la conexion de mongoose a mongodb

---
+++ app/config/conexion.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const CONFIG = require('./configuracion');
+
+module.exports={
+    conection : null, 
+    connect :()=>{
+        if(this.conection)return this.connection
+        return mongoose.connect(CONFIG.DB)
+        .then(conn =>{
+            this.conection=conn
+            console.log('conectado a la base de datos')
+        })
+        .catch (e => console.log('error en la conexion',e))
+    }
+}
